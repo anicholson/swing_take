0fbdc789d2309f039222c0357fbeabb40e28bd99
Andy Nicholson <andrew@anicholson.net>
2012-05-19 08:47:10 +1000

    Get Unit tests working under Sinatra

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 
+get '/spec/javascripts/:file' do
+  send_file "spec/javascripts/#{params[:file]}"
+end
+
+get '/jasmine-1.2.0/:file' do
+  send_file "spec/jasmine-1.2.0/#{params[:file]}"
+end
+
+get '/ci' do
+send_file 'spec/SpecRunner.html'
+end
 
 get '/' do
 redirect '/index.html'
 end
+
