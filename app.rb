60660c14c9ad90927957383c402a49b7cf6e81b6
Andy Nicholson <andy@coremind.com.au>
2012-06-12 17:09:45 +1000

    Add cache control to pages

--- app.rb
+++ app.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'sinatra'
 
 get '/spec/javascripts/:file' do
+  cache_control :public, :no_cache
   send_file "spec/javascripts/#{params[:file]}"
 end
 
 get '/jasmine-1.2.0/:file' do
+  cache_control :public, :no_cache
   send_file "spec/jasmine-1.2.0/#{params[:file]}"
 end
 
@@ -14,6 +16,7 @@ send_file 'spec/SpecRunner.html'
 end
 
 get '/' do
+cache_control :public, :must_revalidate, :max_age=>3600
 send_file 'public/index.html'
 end
 
